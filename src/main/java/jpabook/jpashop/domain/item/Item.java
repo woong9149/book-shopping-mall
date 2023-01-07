78ac16a5e654fcd11c1a4ad38ebbcfef5728d5c1
woong9149 <sewoong0409@naver.com>
2023-01-07 23:53:18 +0900

    220107 modify item entity

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -27,4 +27,26 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 비즈니스 로직
+    // 데이터를 가지고 있는 곳에서 비즈니스 메소드를 만드는것이 응집력이있고, 객체지향적이다.
+
+    /**
+     * stock 증가
+     */
+    public void addStockQuantity(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 증가
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0 ) {
+            throw  new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
+
 }
